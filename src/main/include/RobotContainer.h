e44d070a91e1aa8db4b265474c93c8aaf062d793
K.Sielski <kris@sielski.com>
2022-11-18 15:48:08 -0500

    Initial code clean up

--- src/main/include/RobotContainer.h
+++ src/main/include/RobotContainer.h
@@ -9,13 +9,7 @@
 #include "commands/ExampleCommand.h"
 #include "subsystems/ExampleSubsystem.h"
 
-/**
- * This class is where the bulk of the robot should be declared.  Since
- * Command-based is a "declarative" paradigm, very little robot logic should
- * actually be handled in the {@link Robot} periodic methods (other than the
- * scheduler calls).  Instead, the structure of the robot (including subsystems,
- * commands, and button mappings) should be declared here.
- */
+
 class RobotContainer {
  public:
   RobotContainer();
