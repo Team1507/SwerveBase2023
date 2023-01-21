9b45d0b7e71d3f53e7bf69712c04f7cbd1f824cb
Warlocks1507Programmer <programmers@warlocks1507.com>
2023-01-21 15:36:27 -0500

    Auto Over And Back

--- src/main/cpp/commands/CmdAutoBalance.cpp
+++ src/main/cpp/commands/CmdAutoBalance.cpp
@@ -5,7 +5,7 @@
 #define ERROR 2
 
 float prevTilt = 0;
-CmdAutoBalance::CmdAutoBalance(float power, float heading) 
+CmdAutoBalance::CmdAutoBalance() 
 {
   AddRequirements({ &m_container.m_drivetrain });
   
@@ -16,7 +16,7 @@ void CmdAutoBalance::Initialize()
   prevTilt = 0;
   m_speed = .2;
   m_timer.Reset();
-  m_offDelayCount = 45;
+  m_offDelayCount = 40;
   m_speedDelayCount = 20;
   m_rev = false;
   m_initial = false;    
@@ -24,50 +24,47 @@ void CmdAutoBalance::Initialize()
 
 void CmdAutoBalance::Execute() 
 {
+  
+  float currTilt = m_container.m_drivetrain.GetGyroRoll();
+
+  if(m_container.m_drivetrain.GetGyroRoll() > 0 + ERROR && !m_rev)
+  {
+    m_container.m_drivetrain.RobotcentricDrive( m_speed, 0 ,0.0);   
+    std::cout<<"Running"<<std::endl;
+  }
+
+  if(!m_initial)
+  {
+    m_speedDelayCount--;
+    if(m_speedDelayCount <= 0)
+    {
+      m_initial = true;
+    }
+  }
+
+  if(m_initial)
+  {
+    m_speed = .15;
+  }
+
+  //std::cout<<m_container.m_drivetrain.GetGyroRoll()<<std::endl;
+
+  if(prevTilt > 0 && currTilt < 0 )
+  {
+    m_container.m_drivetrain.RobotcentricDrive(-.25 ,0 ,0.0);
+    m_rev = true;
+  }
+  if(m_rev)
+  {
+    m_offDelayCount--;
+    if(m_offDelayCount <= 0)
+    {
+        m_container.m_drivetrain.RobotcentricDrive(0 ,0 ,0.0);
+        m_container.m_drivetrain.ForcePark();
+    }
+  }
     
-    float currTilt = m_container.m_drivetrain.GetGyroRoll();
-
-    if(m_container.m_drivetrain.GetGyroRoll() > 0 + ERROR && !m_rev)
-    {
-      m_container.m_drivetrain.RobotcentricDrive(m_speed ,0 ,0.0);   
-      std::cout<<"Running"<<std::endl;
-    }
-
-    if(!m_initial)
-    {
-      m_speedDelayCount--;
-      if(m_speedDelayCount <= 0)
-      {
-        m_initial = true;
-      }
-    }
-
-    if(m_initial)
-    {
-      m_speed = .15;
-    }
-
-    //std::cout<<m_container.m_drivetrain.GetGyroRoll()<<std::endl;
-
-      if(prevTilt > 0 && currTilt < 0 )
-      {
-        m_container.m_drivetrain.RobotcentricDrive(-.25 ,0 ,0.0);
-        m_rev = true;
-      }
-
-      if(m_rev)
-      {
-            m_offDelayCount--;
-            if(m_offDelayCount <= 0)
-            {
-                m_container.m_drivetrain.RobotcentricDrive(0 ,0 ,0.0);
-                m_container.m_drivetrain.ForcePark();
-            }
-      }
-
-
-      
-      prevTilt = currTilt;
+  prevTilt = currTilt;
 }
 
 void CmdAutoBalance::End(bool interrupted) {}
@@ -75,9 +72,15 @@ void CmdAutoBalance::End(bool interrupted) {}
 bool CmdAutoBalance::IsFinished() 
 {
 
-
-    
-    return false;
+    if(m_rev && (m_offDelayCount < 0))
+    {
+        return true;
+    }
+    else
+    {
+      return false;
+    }
+      
     
     
 }
